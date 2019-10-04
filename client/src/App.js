a9b22a17d2d046b309adcd9cafddd34009b5045d
Bayron Puac <bayronpuac@gmail.com>
2019-10-04 10:35:03 -0500

    Added new lines of code to the App .js file.

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,25 @@ import React from 'react';
 import './App.css';
 import axios from "axios";
 
-class App extends React.Component{
-  state = {
-    players: []
+const App = () => {
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
   };
-  
-  componentDidMount() {
-    axios
-    .get("http://localhost:5000/api/players")
-    .then (res => {
-      console.log(res.data);
-      this.setState({
-        players: res.data
-      });
-    })
-    .catch(err => console.log(err));
-  }
-  
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
-}
+  return (
+    <nav className="navbar">
+     <h1>Player List</h1>
+      <div onClick={toggleMode} className="dark-mode__toggle">
+        <div
+
+          className={darkMode ? 'toggle toggled' : 'toggle'}
+        />
+      </div>  
+    </nav>
+  );
+};
+
 
 
 
